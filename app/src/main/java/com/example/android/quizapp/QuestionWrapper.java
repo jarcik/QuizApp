31a18d57474ce1c081a8cabce304d1a0f21cae8d
jarcik <j.otmanova@gmail.com>
2018-09-06 17:12:28 +0200

    first commit

---
+++ app/src/main/java/com/example/android/quizapp/QuestionWrapper.java
@@ -0,0 +1,14 @@
+package com.example.android.quizapp;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/*
+General wrapper for one question in quiz app
+ */
+public class QuestionWrapper extends View {
+    public QuestionWrapper(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
